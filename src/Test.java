6a7314a5b5fc059d5c072e4d4ea0931eb75db9a6
Janusz <jderma@wp.pl>
2012-10-28 18:38:58 +0100

    Pierwszy save

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Test {
+
+	public static void main(String[] args){
+		
+	}
+	
+	public List<Integer> sortCollection(List<Integer> list){
+		Collections.sort(list);
+		return list;
+	}
+	
+	public void nowaMetoda(){}
+	
+}
